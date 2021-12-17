32b5e682aeebb1b6b5f94c18510c114bd526fa1a
Pedro Janeiro <pjaneiro@whitesmith.co>
2021-12-17 00:38:27 +0000

    Eighth day

--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
-	"github.com/pjaneiro/advent-of-code-2019/amplificationcircuit"
 	"github.com/pjaneiro/advent-of-code-2019/a1202programalarm"
+	"github.com/pjaneiro/advent-of-code-2019/amplificationcircuit"
 	"github.com/pjaneiro/advent-of-code-2019/crossedwires"
 	"github.com/pjaneiro/advent-of-code-2019/securecontainer"
+	"github.com/pjaneiro/advent-of-code-2019/spaceimageformat"
 	"github.com/pjaneiro/advent-of-code-2019/sunnywithachanceofasteroids"
 	"github.com/pjaneiro/advent-of-code-2019/thetyrannyoftherocketequation"
 	"github.com/pjaneiro/advent-of-code-2019/universalorbitmap"
@@ -42,6 +43,10 @@ func main() {
 
 	timer = time.Now()
 	amplificationcircuit.Run()
+	fmt.Printf("Time elapsed: %v\n\n", time.Since(timer))
+
+	timer = time.Now()
+	spaceimageformat.Run()
 	fmt.Printf("Time elapsed: %v\n\n", time.Since(timer))
 
 	fmt.Printf("Total time elapsed: %v\n", time.Since(timerAll))
