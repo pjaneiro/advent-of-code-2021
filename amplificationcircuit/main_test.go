69d3f9ff997fb762e5bbcb4ef8058eca189b4ecb
Pedro Janeiro <pjaneiro@whitesmith.co>
2021-12-17 00:35:05 +0000

    Seventh day

---
+++ amplificationcircuit/main_test.go
@@ -0,0 +1,79 @@
+package amplificationcircuit_test
+
+import (
+	. "github.com/pjaneiro/advent-of-code-2019/amplificationcircuit"
+	"testing"
+)
+
+func TestChallenge1(t *testing.T) {
+	var testCases = []struct {
+		name     string
+		input    []int
+		expected int
+		error    bool
+	}{
+		{
+			name:     "Example 1",
+			input:    []int{3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0},
+			expected: 43210,
+			error:    false,
+		}, {
+			name:     "Example 2",
+			input:    []int{3, 23, 3, 24, 1002, 24, 10, 24, 1002, 23, -1, 23, 101, 5, 23, 23, 1, 24, 23, 23, 4, 23, 99, 0, 0},
+			expected: 54321,
+			error:    false,
+		}, {
+			name:     "Example 3",
+			input:    []int{3, 31, 3, 32, 1002, 32, 10, 32, 1001, 31, -2, 31, 1007, 31, 0, 33, 1002, 33, 7, 33, 1, 33, 31, 31, 1, 32, 31, 31, 4, 31, 99, 0, 0, 0},
+			expected: 65210,
+			error:    false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			actual, err := Challenge1(tc.input)
+			if err != nil && tc.error == false {
+				t.Errorf("Challenge1(%v) threw '%v', want %d", tc.input, err, tc.expected)
+			} else if err == nil && tc.error == true {
+				t.Errorf("Challenge1(%v) = %d, want to throw", tc.input, actual)
+			} else if actual != tc.expected {
+				t.Errorf("Challenge1(%v) = %d, want %d", tc.input, actual, tc.expected)
+			}
+		})
+	}
+}
+
+func TestChallenge2(t *testing.T) {
+	var testCases = []struct {
+		name     string
+		input    []int
+		expected int
+		error    bool
+	}{
+		{
+			name:     "Example 1",
+			input:    []int{3, 26, 1001, 26, -4, 26, 3, 27, 1002, 27, 2, 27, 1, 27, 26, 27, 4, 27, 1001, 28, -1, 28, 1005, 28, 6, 99, 0, 0, 5},
+			expected: 139629729,
+			error:    false,
+		}, {
+			name:     "Example 2",
+			input:    []int{3, 52, 1001, 52, -5, 52, 3, 53, 1, 52, 56, 54, 1007, 54, 5, 55, 1005, 55, 26, 1001, 54, -5, 54, 1105, 1, 12, 1, 53, 54, 53, 1008, 54, 0, 55, 1001, 55, 1, 55, 2, 53, 55, 53, 4, 53, 1001, 56, -1, 56, 1005, 56, 6, 99, 0, 0, 0, 0, 10},
+			expected: 18216,
+			error:    false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			actual, err := Challenge2(tc.input)
+			if err != nil && tc.error == false {
+				t.Errorf("Challenge2(%v) threw '%v', want %d", tc.input, err, tc.expected)
+			} else if err == nil && tc.error == true {
+				t.Errorf("Challenge2(%v) = %d, want to throw", tc.input, actual)
+			} else if actual != tc.expected {
+				t.Errorf("Challenge2(%v) = %d, want %d", tc.input, actual, tc.expected)
+			}
+		})
+	}
+}
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/pjaneiro/advent-of-code-2019/amplificationcircuit"
 	"github.com/pjaneiro/advent-of-code-2019/a1202programalarm"
 	"github.com/pjaneiro/advent-of-code-2019/crossedwires"
 	"github.com/pjaneiro/advent-of-code-2019/securecontainer"
@@ -37,6 +38,10 @@ func main() {
 
 	timer = time.Now()
 	universalorbitmap.Run()
+	fmt.Printf("Time elapsed: %v\n\n", time.Since(timer))
+
+	timer = time.Now()
+	amplificationcircuit.Run()
 	fmt.Printf("Time elapsed: %v\n\n", time.Since(timer))
 
 	fmt.Printf("Total time elapsed: %v\n", time.Since(timerAll))
