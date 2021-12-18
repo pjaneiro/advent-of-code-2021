2a20ef884da40c70286c86ea0d388dd5640a7172
Pedro Janeiro <pjaneiro@whitesmith.co>
2021-12-18 01:24:08 +0000

    Reduce running time of day 23 of 2020 by using container/ring

--- 2020/crabcups/main.go
+++ 2020/crabcups/main.go
@@ -1,105 +1,72 @@
 package crabcups
 
 import (
+	"container/ring"
 	"fmt"
 	"strconv"
 )
 
-type Edges struct {
-	Prev int
-	Next int
+func createLinkedList(data []int, size int) []*ring.Ring {
+	result := ring.New(size)
+	var pointers []*ring.Ring = make([]*ring.Ring, size+1)
+	for i := 0; i < len(data); i++ {
+		result.Value = data[i]
+		pointers[data[i]] = result
+		result = result.Next()
+	}
+	if size > len(data) {
+		for i := len(data) + 1; i <= size; i++ {
+			result.Value = i
+			pointers[i] = result
+			result = result.Next()
+		}
+	}
+	return pointers
 }
 
-func createLinkedList(data []int, size int) map[int]Edges {
-	var result map[int]Edges = make(map[int]Edges, size)
-	for i := 0; i < 9; i++ {
-		result[data[i]] = Edges{Prev: data[(i+8)%9], Next: data[(i+10)%9]}
-	}
-	if size > 9 {
-		result[10] = Edges{Prev: data[len(data)-1], Next: 11}
-		for i := 11; i < size; i++ {
-			result[i] = Edges{Prev: i - 1, Next: i + 1}
+func solve(cups []int, size int, steps int) []*ring.Ring {
+	pointers := createLinkedList(cups, size)
+	cur := pointers[cups[0]]
+
+	for move := 0; move < steps; move++ {
+		toMove := cur.Unlink(3)
+
+		destValue := (cur.Value).(int) - 1
+		if destValue < 1 {
+			destValue = size
 		}
-		result[1000000] = Edges{Prev: 999999, Next: data[0]}
-		result[data[0]] = Edges{Prev: 1000000, Next: result[data[0]].Next}
-		result[data[8]] = Edges{Prev: result[data[8]].Prev, Next: 10}
-	}
-	return result
-}
-
-func Challenge1(cups []int) (string, error) {
-	list := createLinkedList(cups, 9)
-
-	cur := cups[0]
-	for move := 0; move < 100; move++ {
-		next1 := list[cur].Next
-		next2 := list[next1].Next
-		next3 := list[next2].Next
-
-		dest := cur - 1
-		if dest < 1 {
-			dest = 9
-		}
-		for dest == next1 || dest == next2 || dest == next3 {
-			dest = dest - 1
-			if dest < 1 {
-				dest = 9
+		for destValue == (toMove.Value).(int) || destValue == (toMove.Next().Value).(int) || destValue == (toMove.Prev().Value).(int) {
+			destValue = destValue - 1
+			if destValue < 1 {
+				destValue = size
 			}
 		}
 
-		list[list[next3].Next] = Edges{Prev: cur, Next: list[list[next3].Next].Next}
-		list[cur] = Edges{Prev: list[cur].Prev, Next: list[next3].Next}
+		dest := pointers[destValue]
+		dest.Link(toMove)
 
-		list[list[dest].Next] = Edges{Prev: next3, Next: list[list[dest].Next].Next}
-		list[next3] = Edges{Prev: list[next3].Prev, Next: list[dest].Next}
-		list[next1] = Edges{Prev: dest, Next: next2}
-		list[dest] = Edges{Prev: list[dest].Prev, Next: next1}
-
-		cur = list[cur].Next
+		cur = cur.Next()
 	}
+	return pointers
+}
+
+func Challenge1(cups []int) (string, error) {
+	pointers := solve(cups, 9, 100)
 
 	result := ""
 
-	for cur := list[1].Next; cur != 1; cur = list[cur].Next {
-		result = result + strconv.Itoa(cur)
+	for cur := pointers[1].Next(); cur.Value != 1; cur = cur.Next() {
+		result = result + strconv.Itoa((cur.Value).(int))
 	}
 
 	return result, nil
 }
 
 func Challenge2(cups []int) (string, error) {
-	list := createLinkedList(cups, 1000000)
+	pointers := solve(cups, 1000000, 10000000)
 
-	cur := cups[0]
-	for move := 0; move < 10000000; move++ {
-		next1 := list[cur].Next
-		next2 := list[next1].Next
-		next3 := list[next2].Next
-
-		dest := cur - 1
-		if dest < 1 {
-			dest = 1000000
-		}
-		for dest == next1 || dest == next2 || dest == next3 {
-			dest = dest - 1
-			if dest < 1 {
-				dest = 1000000
-			}
-		}
-
-		list[list[next3].Next] = Edges{Prev: cur, Next: list[list[next3].Next].Next}
-		list[cur] = Edges{Prev: list[cur].Prev, Next: list[next3].Next}
-
-		list[list[dest].Next] = Edges{Prev: next3, Next: list[list[dest].Next].Next}
-		list[next3] = Edges{Prev: list[next3].Prev, Next: list[dest].Next}
-		list[next1] = Edges{Prev: dest, Next: next2}
-		list[dest] = Edges{Prev: list[dest].Prev, Next: next1}
-
-		cur = list[cur].Next
-	}
-
-	res1, res2 := list[1].Next, list[list[1].Next].Next
-	prod := int64(int64(res1) * int64(res2))
+	res1, res2 := pointers[1].Next(), pointers[1].Next().Next()
+	prod := int64(int64((res1.Value).(int)) * int64((res2.Value).(int)))
 
 	return strconv.FormatInt(prod, 10), nil
 }
