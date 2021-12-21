584d2ec5a78eb24c4109d2ade3b80a9d24f2cc00
Pedro Janeiro <pjaneiro@whitesmith.co>
2021-12-21 23:28:00 +0000

    2019 - Fix ninth day

--- 2019/sensorboost/main.go
+++ 2019/sensorboost/main.go
@@ -9,31 +9,60 @@ import (
 	"strings"
 )
 
-func readLines(path string) ([]int64, error) {
+const OUTPUT = true
+const DEBUG = false
+
+const PARAMMODE_POSITION = 0
+const PARAMMODE_IMMEDIATE = 1
+const PARAMMODE_RELATIVE = 2
+
+const OPCODE_ADD int64 = 1
+const OPCODE_MULT int64 = 2
+const OPCODE_WRITE int64 = 3
+const OPCODE_PRINT int64 = 4
+const OPCODE_JUMP_IF_TRUE int64 = 5
+const OPCODE_JUMP_IF_FALSE int64 = 6
+const OPCODE_LESS_THAN int64 = 7
+const OPCODE_EQUALS int64 = 8
+const OPCODE_ADJUST_RB int64 = 9
+const OPCODE_HALT int64 = 99
+
+func readLines(path string) (map[int64]int64, error) {
 	file, err := os.Open(path)
 	if err != nil {
 		return nil, err
 	}
 	defer file.Close()
 
-	var lines []int64
+	var lines map[int64]int64 = make(map[int64]int64)
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
-		for _, v := range strings.Split(line, ",") {
+		for i, v := range strings.Split(line, ",") {
 			n, err := strconv.ParseInt(v, 10, 64)
 			if err != nil {
 				return lines, errors.New("something went wrong scanning int")
 			}
-			lines = append(lines, n)
+			lines[int64(i)] = n
 		}
 	}
 
 	return lines, nil
 }
 
-func intCode(data []int64, input []int64) (int64, error) {
-	var ip, ii, out int64 = 0, 0, 0
+func readParam(data map[int64]int64, parammode int64, ip int64, pos int64, rb int64) int64 {
+	if parammode == PARAMMODE_POSITION {
+		return data[ip+pos]
+	} else if parammode == PARAMMODE_IMMEDIATE {
+		return ip + pos
+	} else if parammode == PARAMMODE_RELATIVE {
+		return rb + data[ip+pos]
+	}
+	return 0
+}
+
+func intCode(data map[int64]int64, input []int64) (int64, error) {
+	var ip, ii, out, rb int64 = 0, 0, 0, 0
 	for true {
 		opcode, instruction := data[ip]%100, data[ip]/100
 		parammodes := []int64{}
@@ -45,130 +74,76 @@ func intCode(data []int64, input []int64) (int64, error) {
 			parammodes = append(parammodes, 0)
 		}
 		switch opcode {
-		case 1:
-			var val1, val2 int64 = 0, 0
-			if parammodes[0] == 0 {
-				val1 = data[data[ip+1]]
-			} else {
-				val1 = data[ip+1]
-			}
-			if parammodes[1] == 0 {
-				val2 = data[data[ip+2]]
-			} else {
-				val2 = data[ip+2]
-			}
-			data[data[ip+3]] = val1 + val2
+		case OPCODE_ADD:
+			var val1, val2, val3 int64 = readParam(data, parammodes[0], ip, 1, rb), readParam(data, parammodes[1], ip, 2, rb), readParam(data, parammodes[2], ip, 3, rb)
+			data[val3] = data[val1] + data[val2]
 			ip += 4
-		case 2:
-			var val1, val2 int64 = 0, 0
-			if parammodes[0] == 0 {
-				val1 = data[data[ip+1]]
-			} else {
-				val1 = data[ip+1]
-			}
-			if parammodes[1] == 0 {
-				val2 = data[data[ip+2]]
-			} else {
-				val2 = data[ip+2]
-			}
-			data[data[ip+3]] = val1 * val2
+		case OPCODE_MULT:
+			var val1, val2, val3 int64 = readParam(data, parammodes[0], ip, 1, rb), readParam(data, parammodes[1], ip, 2, rb), readParam(data, parammodes[2], ip, 3, rb)
+			data[val3] = data[val1] * data[val2]
 			ip += 4
-		case 3:
-			data[data[ip+1]] = input[ii]
+		case OPCODE_WRITE:
+			var val1 int64 = readParam(data, parammodes[0], ip, 1, rb)
+			data[val1] = input[ii]
 			ip += 2
 			ii++
-		case 4:
-			if parammodes[0] == 0 {
-				out = data[data[ip+1]]
-			} else {
-				out = data[ip+1]
+		case OPCODE_PRINT:
+			out = data[readParam(data, parammodes[0], ip, 1, rb)]
+			if OUTPUT {
+				fmt.Println(out)
 			}
-			fmt.Println(out)
 			ip += 2
-		case 5:
-			var val1, val2 int64 = 0, 0
-			if parammodes[0] == 0 {
-				val1 = data[data[ip+1]]
-			} else {
-				val1 = data[ip+1]
-			}
-			if parammodes[1] == 0 {
-				val2 = data[data[ip+2]]
-			} else {
-				val2 = data[ip+2]
-			}
-			if val1 != 0 {
-				ip = val2
+		case OPCODE_JUMP_IF_TRUE:
+			var val1, val2 int64 = readParam(data, parammodes[0], ip, 1, rb), readParam(data, parammodes[1], ip, 2, rb)
+			if data[val1] != 0 {
+				ip = data[val2]
 			} else {
 				ip += 3
 			}
-		case 6:
-			var val1, val2 int64 = 0, 0
-			if parammodes[0] == 0 {
-				val1 = data[data[ip+1]]
-			} else {
-				val1 = data[ip+1]
-			}
-			if parammodes[1] == 0 {
-				val2 = data[data[ip+2]]
-			} else {
-				val2 = data[ip+2]
-			}
-			if val1 == 0 {
-				ip = val2
+		case OPCODE_JUMP_IF_FALSE:
+			var val1, val2 int64 = readParam(data, parammodes[0], ip, 1, rb), readParam(data, parammodes[1], ip, 2, rb)
+			if data[val1] == 0 {
+				ip = data[val2]
 			} else {
 				ip += 3
 			}
-		case 7:
-			var val1, val2 int64 = 0, 0
-			if parammodes[0] == 0 {
-				val1 = data[data[ip+1]]
+		case OPCODE_LESS_THAN:
+			var val1, val2, val3 int64 = readParam(data, parammodes[0], ip, 1, rb), readParam(data, parammodes[1], ip, 2, rb), readParam(data, parammodes[2], ip, 3, rb)
+			if data[val1] < data[val2] {
+				data[val3] = 1
 			} else {
-				val1 = data[ip+1]
-			}
-			if parammodes[1] == 0 {
-				val2 = data[data[ip+2]]
-			} else {
-				val2 = data[ip+2]
-			}
-			if val1 < val2 {
-				data[data[ip+3]] = 1
-			} else {
-				data[data[ip+3]] = 0
+				data[val3] = 0
 			}
 			ip += 4
-		case 8:
-			var val1, val2 int64 = 0, 0
-			if parammodes[0] == 0 {
-				val1 = data[data[ip+1]]
+		case OPCODE_EQUALS:
+			var val1, val2, val3 int64 = readParam(data, parammodes[0], ip, 1, rb), readParam(data, parammodes[1], ip, 2, rb), readParam(data, parammodes[2], ip, 3, rb)
+			if data[val1] == data[val2] {
+				data[val3] = 1
 			} else {
-				val1 = data[ip+1]
-			}
-			if parammodes[1] == 0 {
-				val2 = data[data[ip+2]]
-			} else {
-				val2 = data[ip+2]
-			}
-			if val1 == val2 {
-				data[data[ip+3]] = 1
-			} else {
-				data[data[ip+3]] = 0
+				data[val3] = 0
 			}
 			ip += 4
-		case 99:
+		case OPCODE_ADJUST_RB:
+			var offset int64 = readParam(data, parammodes[0], ip, 1, rb)
+			rb = rb + data[offset]
+			ip += 2
+		case OPCODE_HALT:
 			return out, nil
 		default:
+			if DEBUG {
+				fmt.Printf("Tried to use opcode %d\n", opcode)
+			}
 			return 0, errors.New("invalid opcode")
 		}
 	}
 	return 0, errors.New("something went wrong")
 }
 
-func Challenge1(data []int64, input []int64) (int64, error) {
-	return intCode(data, input)
+func Challenge1(data map[int64]int64) (int64, error) {
+	return intCode(data, []int64{1})
 }
 
-func Challenge2(data []int64, input []int64) (int64, error) {
+func Challenge2(data map[int64]int64, input []int64) (int64, error) {
 	return intCode(data, input)
 }
 
@@ -181,18 +156,23 @@ func Run() {
 	}
 
 	var result int64
-	var cpy []int64 = make([]int64, len(data))
 
-	copy(cpy, data)
-	result, err = Challenge1(cpy, []int64{1})
+	var cpy1 map[int64]int64 = make(map[int64]int64)
+	for k, v := range data {
+		cpy1[k] = v
+	}
+	result, err = Challenge1(cpy1)
 	if err != nil {
 		fmt.Printf("Error running challenge 1: %v\n", err)
 	} else {
 		fmt.Printf("Challenge 1: %d\n", result)
 	}
 
-	copy(cpy, data)
-	result, err = Challenge2(cpy, []int64{5})
+	var cpy2 map[int64]int64 = make(map[int64]int64)
+	for k, v := range data {
+		cpy2[k] = v
+	}
+	result, err = Challenge2(cpy2, []int64{2})
 	if err != nil {
 		fmt.Printf("Error running challenge 2: %v\n", err)
 	} else {
--- 2019/sensorboost/main_test.go
+++ 2019/sensorboost/main_test.go
@@ -8,28 +8,24 @@ import (
 func TestChallenge1(t *testing.T) {
 	var testCases = []struct {
 		name     string
-		data     []int64
-		input    []int64
+		input    map[int64]int64
 		expected int64
 		error    bool
 	}{
 		{
 			name:     "Example 1",
-			data:     []int64{109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99},
-			input:    []int64{1},
-			expected: 43210,
+			input:    map[int64]int64{0: 109, 1: 1, 2: 204, 3: -1, 4: 1001, 5: 100, 6: 1, 7: 100, 8: 1008, 9: 100, 10: 16, 11: 101, 12: 1006, 13: 101, 14: 0, 15: 99},
+			expected: 99,
 			error:    false,
 		}, {
 			name:     "Example 2",
-			data:     []int64{1102,34915192,34915192,7,4,7,99,0},
-			input:    []int64{1},
-			expected: 54321,
+			input:    map[int64]int64{0: 1102, 1: 34915192, 2: 34915192, 3: 7, 4: 4, 5: 7, 6: 99, 7: 0},
+			expected: 1219070632396864,
 			error:    false,
 		}, {
 			name:     "Example 3",
-			data:     []int64{104,1125899906842624,99},
-			input:    []int64{1},
-			expected: 65210,
+			input:    map[int64]int64{0: 104, 1: 1125899906842624, 2: 99},
+			expected: 1125899906842624,
 			error:    false,
 		},
 	}
